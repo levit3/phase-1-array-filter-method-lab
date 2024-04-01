6461e6abcd4d11a49fe8137e54fca848aca3322a
Levi Mwendwa <lvmwendwa4@gmail.com>
2024-04-01 12:30:35 +0300

    adds various filter functions

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+function findMatching(array, string) {
+  let allDrivers = array.filter(
+    (driver) => driver.toLowerCase() === string.toLowerCase()
+  );
+  return allDrivers;
+}
+function fuzzyMatch(array, string) {
+  let x = array.filter(
+    (driver) => driver.split("", string.length).join("") === string
+  );
+  return x;
+}
+function matchName(array, string) {
+  let x = array.filter((driver) => driver["name"] === string);
+  return x;
+}
